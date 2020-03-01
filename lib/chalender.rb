faf8bf573cb2807a50802d563487a1cd61c12b06
David Georgiou <david.georgiou@gmail.com>
2020-03-01 22:29:28 +0000

    highlight day of month

--- lib/chalender.rb
+++ lib/chalender.rb
@@ -1,36 +1,60 @@
-require "chalender/version"
+require 'chalender/version'
 require 'thor'
+require 'date'
 
 module Chalender
   class Error < StandardError; end
   
   class CLI < Thor
-    desc 'time', 'tells the time!'
-    def time
-      puts Time.now
-    end
-
-    desc 'print-squares [number]', 'prints squares'
-    def print_squares(num = 14, max_columns = 7)
-      puts header
-      num = num.to_i
-      max_columns = max_columns.to_i
-      remainder = num % 7
-      rows = num / max_columns
-      while rows > 0
-        puts Array.new(max_columns + 1, '[]').join(' ')
-        rows = rows - 1
+    desc 'cal', 'displays a visual calendar'
+    def cal
+      header
+      array_pad = Array.new(month_start_weekday - 1, '  ')
+      days_with_padding = array_pad.concat [*'01'..last_day.to_s]
+      days_with_padding.each_slice(7) do |week|
+        puts highlight_cal_day(week).join(' ')
       end
-      puts Array.new(remainder, '[]').join(' ') unless remainder.zero?
     end
-
 
     private
 
-    def header(padding = 5)
-      padding = Array.new(padding, ' ').join
-      date = Time.now.strftime('%B %Y')
-      puts "#{padding}#{date}"
+    def highlight_cal_day(week)
+      week.collect do |day|
+        if day.to_i == cal_day
+          "\e[7m#{day}\e[27m"
+        else
+          day
+        end
+      end
+    end
+
+    def month_start_weekday
+      Date.new(year, month, 1).cwday
+    end
+
+    def header
+      puts "#{padding}#{Date.today.strftime('%B %Y')}"
+      puts 'Mo Tu We Th Fr Sa Su'
+    end
+
+    def padding(amount = 5)
+      Array.new(amount, ' ').join
+    end
+
+    def last_day
+      Date.new(year, month, -1).day
+    end
+
+    def cal_day
+      Date.today.day
+    end
+
+    def month
+      Date.today.month
+    end
+
+    def year
+      Date.today.year
     end
   end
 end
