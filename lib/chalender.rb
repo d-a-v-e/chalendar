637b087a76643292d5591116d4c6b214b0646800
David Georgiou <david.georgiou@gmail.com>
2020-03-01 19:50:01 +0000

    cal header

--- lib/chalender.rb
+++ lib/chalender.rb
@@ -12,15 +12,25 @@ module Chalender
 
     desc 'print-squares [number]', 'prints squares'
     def print_squares(num = 14, max_columns = 7)
+      puts header
       num = num.to_i
       max_columns = max_columns.to_i
       remainder = num % 7
       rows = num / max_columns
       while rows > 0
-        puts Array.new(max_columns + 1, '[]')
+        puts Array.new(max_columns + 1, '[]').join(' ')
         rows = rows - 1
       end
-      puts Array.new(remainder + 1, '[]') unless remainder.zero?
+      puts Array.new(remainder, '[]').join(' ') unless remainder.zero?
+    end
+
+
+    private
+
+    def header(padding = 5)
+      padding = Array.new(padding, ' ').join
+      date = Time.now.strftime('%B %Y')
+      puts "#{padding}#{date}"
     end
   end
 end
